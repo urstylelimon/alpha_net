f8201c930507a5c8477a96f15510e97a564c062e
Mh Limon <mhlimon571@gmail.com>
2025-12-04 01:53:56 +0600

    implement api url

--- transcription/urls.py
+++ transcription/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'sessions', views.SessionViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/', include(router.urls)),
 ]
