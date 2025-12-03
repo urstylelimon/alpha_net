7265eaf6cb395f9610fd34cc87598d08c9a7240e
limon <mhlimon571@gmail.com>
2025-12-04 00:43:17 +0600

    implement model for save time duratration and text

--- transcription/models.py
+++ transcription/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class TranscriptionSession(models.Model):
+
+    start_time = models.DateTimeField(auto_now_add=True)
+
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    duration_seconds = models.FloatField(default=0.0)
+
+    final_transcript = models.TextField(blank=True)
