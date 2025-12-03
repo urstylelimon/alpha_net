f3b82f678d3b73773774a2ef858584ad28380812
Mh Limon <mhlimon571@gmail.com>
2025-12-04 01:53:31 +0600

    implement serializer and api

--- transcription/views.py
+++ transcription/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import TranscriptionSession
+from .serializers import TranscriptionSessionSerializer
 def index(request):
     return render(request, 'transcription/index.html')
+
+class SessionViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = TranscriptionSession.objects.all().order_by('-start_time')
+    serializer_class = TranscriptionSessionSerializer
