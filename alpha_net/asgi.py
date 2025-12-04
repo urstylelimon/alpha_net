a1998f3ccd529c9c137e50224e63eb1b23bf0175
Mh Limon <mhlimon571@gmail.com>
2025-12-04 14:09:20 +0600

    Optimized code

--- alpha_net/asgi.py
+++ alpha_net/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alpha_net.settings')
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import transcription.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alpha_net.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
+
     "websocket": AuthMiddlewareStack(
         URLRouter(
             transcription.routing.websocket_urlpatterns
