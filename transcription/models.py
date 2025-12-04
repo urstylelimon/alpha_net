9cf408685fa8769eecedabe763267edf23357c49
Mh Limon <mhlimon571@gmail.com>
2025-12-04 16:08:14 +0600

    add new features word count

--- transcription/models.py
+++ transcription/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class TranscriptionSession(models.Model):
-
     start_time = models.DateTimeField(auto_now_add=True)
-
     end_time = models.DateTimeField(null=True, blank=True)
-
     duration_seconds = models.FloatField(default=0.0)
-
     final_transcript = models.TextField(blank=True)
+    word_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Session {self.id} - {self.start_time.strftime('%Y-%m-%d %H:%M')}"
